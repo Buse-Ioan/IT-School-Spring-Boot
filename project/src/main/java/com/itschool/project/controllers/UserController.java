0d896f936119c2855727dd5e590e7995ab5a32bb
buse_ <buse.v.ioan@gmail.com>
2024-08-29 21:18:15 +0300

    update

--- project/src/main/java/com/itschool/project/controllers/UserController.java
+++ project/src/main/java/com/itschool/project/controllers/UserController.java
@@ -1,23 +1,31 @@
 package com.itschool.project.controllers;
 
-import com.itschool.project.models.User;
+import com.itschool.project.models.dtos.UserDTO;
 import com.itschool.project.services.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @PostMapping("/api/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        return ResponseEntity.ok(null);
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.createUser(userDTO));
+    }
+
+    @GetMapping("/api/users")
+    public ResponseEntity<List<UserDTO>> getUsers() {
+        return ResponseEntity.ok(userService.getUsers());
     }
 }
