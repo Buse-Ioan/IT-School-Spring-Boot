7c16cd4e3a53606be62442d206f282d0392c28a3
buse_ <buse.v.ioan@gmail.com>
2024-08-29 21:24:41 +0300

    update

--- project/src/main/java/com/itschool/project/services/UserService.java
+++ project/src/main/java/com/itschool/project/services/UserService.java
@@ -1,8 +1,11 @@
 package com.itschool.project.services;
 
-import com.itschool.project.models.User;
+import com.itschool.project.models.dtos.UserDTO;
+
+import java.util.List;
 
 public interface UserService {
 
-    User createUser(User user);
+    UserDTO createUser(UserDTO userDTO);
+    List<UserDTO> getUsers();
 }
