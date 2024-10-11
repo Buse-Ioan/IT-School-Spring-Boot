a0093eab4ea1ba577de237532fc06d34f592e913
buse_ <buse.v.ioan@gmail.com>
2024-10-11 19:14:22 +0300

    update

---
+++ project/src/test/java/ProjectApplicationTests.java
@@ -0,0 +1,20 @@
+
+import com.itschool.project.ProjectApplication;
+import com.itschool.project.services.GreetingService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectApplication.class);
+        GreetingService greetingService = context.getBean(GreetingService.class);
+
+        String greeting = greetingService.greet();
+        System.out.println(greeting);
+    }
+}
